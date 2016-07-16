0b0c48449049b3903ec04f114715a5f8f66e3d02
Alexandre Ignatov <estaxo@yandex.ru>
2016-07-16 22:47:40 +0300

    Create VerticalLine Class

--- ConsoleSerpent/ConsoleSerpent/Program.cs
+++ ConsoleSerpent/ConsoleSerpent/Program.cs
@@ -12,8 +12,10 @@ namespace ConsoleSerpent {
 			Point p2 = new Point(2, 6, '@');
 			Point p3 = new Point(7, 2, '^');
 
-			HorizontalLine line = new HorizontalLine(4, 7, 12, '&');
-			line.Draw();
+			HorizontalLine line1 = new HorizontalLine(4, 7, 5, '&');
+			line1.Draw();
+			VerticalLine line2 = new VerticalLine(4, 7, 5, '&');
+			line2.Draw();
 
 			Console.ReadLine();
 		}
---
+++ ConsoleSerpent/ConsoleSerpent/VerticalLine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSerpent {
+	class VerticalLine {
+		List<Point> pList;
+
+		public VerticalLine(int x, int y, int length, char symbol) {
+			pList = new List<Point>();
+
+			for (int i = 0; i < length; i++) {
+				Point p = new Point(x, y + i, symbol);
+				pList.Add(p);
+			}
+		}
+
+		public void Draw() {
+			foreach (Point p in pList) {
+				p.Draw();
+			}
+		}
+	}
+}
