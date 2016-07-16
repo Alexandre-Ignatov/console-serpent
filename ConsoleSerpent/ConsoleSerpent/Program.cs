74b1c7e6fafc38d9b17a338370c2184aef29e37c
Alexandre Ignatov <estaxo@yandex.ru>
2016-07-16 21:08:25 +0300

    Add "Draw" function
    Experimenting with procedural programming paradigm

--- ConsoleSerpent/ConsoleSerpent/Program.cs
+++ ConsoleSerpent/ConsoleSerpent/Program.cs
@@ -7,8 +7,15 @@ using System.Threading.Tasks;
 namespace ConsoleSerpent {
 	class Program {
 		static void Main(string[] args) {
-			Console.WriteLine("Hello World");
+
+			Draw(5, 5, '*');
+			Draw(6, 6, '@');
 			Console.ReadLine();
+		}
+
+		static void Draw(int x, int y, char symbol) {
+			Console.SetCursorPosition(x, y);
+			Console.Write(symbol);
 		}
 	}
 }
