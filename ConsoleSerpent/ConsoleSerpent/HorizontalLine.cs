b3cf07b945312cd7f1c8aa5f107bc34d2d7c1ecd
Alexandre Ignatov <estaxo@yandex.ru>
2016-07-16 22:47:21 +0300

    Refactor HorizontalLine Class

--- ConsoleSerpent/ConsoleSerpent/HorizontalLine.cs
+++ ConsoleSerpent/ConsoleSerpent/HorizontalLine.cs
@@ -11,12 +11,8 @@ namespace ConsoleSerpent {
 		public HorizontalLine(int x, int y, int length, char symbol) {
 			pList = new List<Point>();
 
-			int maxX = x + length;
-			int currentX = x;
-
-			for (int i = x; i < maxX; i++) {
-				currentX = x + i;
-				Point p = new Point(currentX, y, symbol);
+			for (int i = 0; i < length; i++) {
+				Point p = new Point(x + i, y, symbol);
 				pList.Add(p);
 			}
 		}
